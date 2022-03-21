a3a5534ff496c573bced2bd5e02b5b0daba05a4e
Tiago-Alves-Dev <tiagoalvesdesouzaoficial@gmail.com>
2022-03-21 09:56:31 -0300

    feat(origin/main):
    run functions and do a publish using the api.

--- app/assets/src/Main.js
+++ app/assets/src/Main.js
@@ -7,12 +7,32 @@ client.metadata().then((metadata) => {
   settings = metadata.settings;
 });
 
-const Main = async () => {
-  const App = document.getElementById("app");
-  let appBody = `<div id="main-content"></div>`;
+const Main = async (requester_ticket) => {
+  const tickets = await client.request(`/api/v2/search.json?query=type:ticket requester_id:${requester_ticket}`);
+  console.log(tickets)
 
-  // Write App
-  App.innerHTML = appBody;
+  const { GetAddressByZipCode, CommentNewTicket } = Core;
+
+  let submit = document.getElementById('submit');
+
+  submit.onclick = async (event) => {
+    event.preventDefault();
+
+    const form = document.getElementById('form');
+
+    const data = await GetAddressByZipCode();
+
+    if (!data === false) {
+
+      const ticket_comment = CommentNewTicket(data)
+
+      client.trigger('publish', ticket_comment);
+
+      form.reset();
+
+    }
+  }
+
 };
 
 export default Main;
