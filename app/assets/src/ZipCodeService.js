9b548ac65216acdb2942d058c18ed97fabd54e28
Tiago-Alves-Dev <tiagoalvesdesouzaoficial@gmail.com>
2022-03-21 05:33:44 -0300

    feat(origin/main):
    adding a button and input for the zip code in the iframe and
    creating a zip code service

---
+++ app/assets/src/ZipCodeService.js
@@ -0,0 +1,24 @@
+const APIAXIOS = axios.create({
+    baseURL: 'https://viacep.com.br/ws/'
+});
+
+const getAddressByZipCode = async (cep) => {
+    const result = await APIAXIOS.get(`${cep}/json/`);
+
+    const { data } = result;
+
+    return { cep, address: data };
+}
+
+const setAddressByZipCode = ({ address: { logradouro, complemento, bairro, localidade, uf } }) => {
+
+    const written_address = ` Logradouro: ${logradouro} Complemento: ${complemento} Bairro: ${bairro} Cidade: ${localidade} Estado: ${uf}`;
+    return written_address;
+}
+
+const ZipCodeService = {
+    getAddressByZipCode,
+    setAddressByZipCode
+}
+
+export default ZipCodeService;
