13b3a0ec7bc87ee7bf0276a579614c8d2838a559
Airam Toscano <airamlobato@gmail.com>
2022-07-08 19:00:48 -0400

    feat: buttom and input

--- app/assets/src/App.js
+++ app/assets/src/App.js
@@ -5,6 +5,21 @@ client.on("app.registered", (e) => {
   client.invoke("resize", { width: "100%", height: "130px" });
 });
 
+let options = {
+  url: '/api/v2/tickets.json',
+  type: 'GET'
+};
+
+client.request(options).then((results) => {
+  console.log("Example 1 call results:", results);
+});
+
+function buttomClick() {
+ButtomList.addEventListener('click', () => {
+  console.log("teste")
+})
+}
 // Create screen context
 import Main from "./Main.js";
 Main();
+buttomClick()
